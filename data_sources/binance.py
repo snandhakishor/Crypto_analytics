2dfdf048a873f3c632791d2013da88dbfbc410e3
snandhakishor <snandhakishor583@gmail.com>
2026-01-15 04:07:55 +0530

    added pyspark, flattened json and converted to pandas df

--- data_sources/binance.py
+++ data_sources/binance.py
@@ -1,10 +1,38 @@
 import requests
 import json
 import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, col
+
+# creating spark session
+spark = SparkSession.builder.getOrCreate()
+
 # def get_binance_price(symbol="BTCUSDT"):
 url = f'https://api.bybit.com/v5/market/funding/history?category=linear&symbol=BTCUSDT'
-# return requests.get(url).json()
 
-df = pd.DataFrame(requests.get(url).json())
+
+data = requests.get(url).json()
+
+# the above data is nested json
+
+df = spark.read.json(spark.sparkContext.parallelize([data]))
 print(df)
 
+# flattening json
+df_flat = df.withColumn("row",explode(col("result.list"))).select(
+    col("retCode"),
+    col("retMsg"),
+    col("result.category").alias("category"),
+    col("row.symbol").alias("symbol"),
+    col("row.fundingRate").cast("double").alias("Funding_rate"),
+    col("row.fundingRateTimestamp").cast("long").alias("funding_rate_timestamp"),
+    col("time").alias("ingestion_time")
+
+)
+
+df_pandas =  df_flat.toPandas()
+df_pandas["ingestion_time"] = pd.to_datetime(df_pandas["ingestion_time"], errors="coerce")
+df_pandas["funding_rate_timestamp"] = pd.to_datetime(
+df_pandas["funding_rate_timestamp"], errors = "coerce"
+                                                     )
+print(df_pandas)
--- data_sources/goingecko.py
+++ data_sources/goingecko.py
@@ -2,11 +2,7 @@ import json
 import requests
 import pandas as pd
 
-url = "https://api.coingecko.com/api/v3/coins/list"
 
-def coin_list():
-    response = requests.get(url)
-    return response.json()
 
 url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1"
 # params = {"ids":"bitcoin,ethereum,solana,dogecoin,cardano,ripple",
@@ -17,5 +13,4 @@ df = pd.DataFrame(response.json())
 df['last_updated'] = pd.to_datetime(df['last_updated'],errors="coerce")
 # print(df.head())
 # print(df.transpose().reset_index().rename(columns={'index':'asset_id'}).head())
-print(df[['current_price', 'id', 'name']].head())
 # print(df.columns)
